61021db69053fe5225448f990a3f794cb933c7a3
asd-35 <saridoganarda35@gmail.com>
2021-07-25 02:02:05 +0300

    game loop and background added

--- GameOfLife/GameOfLife.cpp
+++ GameOfLife/GameOfLife.cpp
@@ -8,10 +8,30 @@ using namespace sf;
 
 int main()
 {
-	VideoMode vm(1920, 1080);
+	VideoMode vm(1200, 960);
 
-	RenderWindow window(vm, "Game Of Life", Style::Resize);
+	RenderWindow window(vm, "Game Of Life", Style::Fullscreen);
+	
+	Texture textureBackground;
+	
+	textureBackground.loadFromFile("graphics/background.png");
 
+	Sprite spriteBackground;
+
+	spriteBackground.setTexture(textureBackground);
+
+	spriteBackground.setPosition(0, 0);
+
+	while (window.isOpen()) {
+		if (Keyboard::isKeyPressed(Keyboard::Escape)) {
+			window.close();
+		} // if esc pressed kill the window
+
+		window.clear(); // clear the window
+
+		window.draw(spriteBackground); // background
+
+		window.display(); // update the window
+	}
 	return 0;
 }
-
